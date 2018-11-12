29a3f7fa2a73c11d0a69c8a16037b49a904c8f74
Rima Jean <rima.jean@gmail.com>
2018-11-12 16:01:42 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2018_11_12_153111) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.datetime "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
